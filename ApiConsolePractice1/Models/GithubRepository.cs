1ec5b3e9905468d3d8bb84ca098e574f3614eb17
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-04-03 12:54:27 +0200

    Add project files.

---
+++ ApiConsolePractice1/Models/GithubRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApiConsolePractice1.Models
+{
+    internal class GithubRepository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+
+        [JsonPropertyName("stargazers_count")]
+        public int Stars { get; set; }
+
+        [JsonPropertyName("forks_count")]
+        public int Forks { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("owner")]
+        public Owner Owner { get; set; }
+    }
+}
