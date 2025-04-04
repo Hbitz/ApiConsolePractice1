ff7b8e5e1868637994330a8a607dc3723e168bf5
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-04-04 13:50:23 +0200

    Moved Owner class

--- ApiConsolePractice1/Models/Owner.cs
+++ ApiConsolePractice1/Models/Owner.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace ApiConsolePractice1
+namespace ApiConsolePractice1.Models
 {
     internal class Owner
     {
